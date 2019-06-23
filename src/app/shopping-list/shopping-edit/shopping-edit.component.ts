c205b6b3020239765579d61a37ccea3c2d90604c
Chris Wang <chriswang@Chriss-MacBook-Pro.local>
2019-06-23 08:59:43 -0400

    Finished section6

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput', { static: false }) ingredientName: ElementRef;
+  @ViewChild('amountInput', { static: false }) ingredientAmount: ElementRef;
+  @Output() ingredientEmitter = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onAddIngredient() {
+    const ingName = this.ingredientName.nativeElement.value;
+    const ingAmount = this.ingredientAmount.nativeElement.value;
+    const ingredient = new Ingredient(ingName, ingAmount);
+    console.log('onAddIngredient reached, name='+ingName+', amount='+ingAmount);
+    this.ingredientEmitter.emit(ingredient);
+  }
+  onDeleteIngredient() {
+    const ingName = this.ingredientName.nativeElement.value;
+    const ingAmount = this.ingredientAmount.nativeElement.value;
+    const ingredient = new Ingredient(ingName, ingAmount);
+    console.log('onDeleteIngredient reached, name='+ingName+', amount='+ingAmount);
+    this.ingredientEmitter.emit(ingredient);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,25 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  // addedIngredient: Ingredient;
+  // ingredients: Ingredient[] = [];
+  // onAddIngredient(ingredientName: string, ingredientAmount: number) {
+  //   console.log("before add, size="+this.ingredients.length)
+  //   this.ingredients.push(new Ingredient(ingredientName, ingredientAmount));
+  // }
+  onIngredientEditted(ingredientAdded: Ingredient) {
+
+    // Delete giving amount on existing ingredient
+    // if (this.ingredients.find(new function sameName(element, index, this.ingredients) {element.name === ingredientAdded.name})) {
+    if (this.ingredients.some(e => e.name === ingredientAdded.name && e.amount >= ingredientAdded.amount)) {  
+      console.log('Deleting '+ingredientAdded.amount + ' for ' + ingredientAdded.name);
+      var element = this.ingredients.filter(e => e.name === ingredientAdded.name)[0];
+      element.amount -= ingredientAdded.amount;
+    } else {
+      console.log("before add, size = "+this.ingredients.length)
+      console.log('Insert element with name: ' + ingredientAdded.name + ' and amount: ' + ingredientAdded.amount);
+      this.ingredients.push(ingredientAdded);
+      console.log("after add, size = "+this.ingredients.length)
+    }
+  }
 }
